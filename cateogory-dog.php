32eaf148365771635ac78038384af6d452beac38
kafetora <kafetora@gmail.com>
2020-06-11 20:11:28 +0900

    Let's start

---
+++ cateogory-dog.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Category Dog
+Description: Limit the categories that can be posted for each user
+Version: 1.0.0
+Author: Takaomi Ogawa
+License: MIT
+Text Domain: category-dog
+*/
+
